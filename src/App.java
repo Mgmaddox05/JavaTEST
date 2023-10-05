82ed6aedbe8b48761bf68a5130fa9b2ef9c24a4c
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-10-05 14:57:24 -0500

    Update 2

--- src/App.java
+++ src/App.java
@@ -1,11 +1,43 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Vector2 v1 = new Vector2(3, 4);
         Vector2 v2 = new Vector2(1, 2, "A 2D vector");
 
-        System.out.println(v1); 
-        System.out.println(v2.getDescription());  // Use 'v2' instead of 'vector2'
-        System.out.println("Vector2 1 Dimension: " + v1.getDimension());
-        System.out.println("Vector2 2 Dimension: " + v2.getDimension());
+        // Clone v1
+        Vector2 v1Clone = v1.clone();
+
+        // Check if the cloned object is equal to the original
+        boolean areEqual = v1.isEqual(v1Clone);
+
+        System.out.println("Original Vector: " + v1);
+        System.out.println("Cloned Vector: " + v1Clone);
+        System.out.println("Are they equal? " + areEqual);
+
+        // Modify the cloned vector using setter methods and check again
+        v1Clone.setDescription("Modified clone");
+        v1Clone.setX(5);
+
+        System.out.println("Modified Cloned Vector: " + v1Clone);
+        System.out.println("Original Vector after modification of clone: " + v1);
+
+        // Calculate and print the magnitude of v2
+        int magnitudeV2 = v2.magnitude();
+        System.out.println("Magnitude of v2: " + magnitudeV2);
+
+        // Add v1 and v2
+        Vector2 sumVector = v1.add(v2);
+        System.out.println("Sum of v1 and v2: " + sumVector);
+
+        // Check if v1 and v2 are equal
+        boolean areEqualVectors = v1.isEqual(v2);
+        System.out.println("Are v1 and v2 equal? " + areEqualVectors);
+
+        // Get the description of v2
+        String descriptionV2 = v2.getDescription();
+        System.out.println("Description of v2: " + descriptionV2);
+
+        // Get the dimension of v2
+        int dimensionV2 = v2.getDimension();
+        System.out.println("Dimension of v2: " + dimensionV2);
     }
 }
--- src/Vector2.java
+++ src/Vector2.java
@@ -14,7 +14,7 @@ public class Vector2 extends MathObject implements Cloneable {
         this.y = y;
     }
 
-    public double magnitude() {
+    public int magnitude() {
         return Math.sqrt(x * x + y * y);
     }
 
@@ -24,6 +24,22 @@ public class Vector2 extends MathObject implements Cloneable {
 
     public boolean isEqual(Vector2 other) {
         return this.x == other.x && this.y == other.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
@@ -37,7 +53,12 @@ public class Vector2 extends MathObject implements Cloneable {
     }
 
     @Override
-    public Vector2 clone() throws CloneNotSupportedException {
-        return (Vector2) super.clone();
+    public Vector2 clone() {
+        try {
+            return (Vector2) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This should not happen since Vector2 implements Cloneable
+            throw new InternalError(e);
+        }
     }
 }
